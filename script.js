68c871851e3141871778f92ad1d8464634a00a1e
prabakarpraba <sprabakaran533@gmail.com>
2024-05-13 08:03:15 +0530

    completed

---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+let displayValue = "";
+//! function for button
+function createButton(value, clickHandler) {
+  let btn = document.createElement("button");
+  btn.innerText = value;
+  btn.addEventListener("click", clickHandler);
+  return btn;
+}
+//! Concatenating new value to existing display value.
+function appendDisplay(value) {
+  displayValue += value;
+  //! set new value as the updated display value
+  document.getElementById("display").value = displayValue;
+}
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = displayValue;
+}
+//! To calculate
+function calculate() {
+  //! setting value to the input field
+  let displayField = document.getElementById("display");
+  try {
+    displayValue = eval(displayValue).toString();
+    displayField.value = displayValue;
+  } catch {
+    displayField.value = "error";
+  }
+}
+//! Creating buttons
+let buttons = [
+  ["clear", clearDisplay],
+  ["<-", () => appendDisplay("<-")],
+  [".", () => appendDisplay(".")],
+  ["*", () => appendDisplay("*")],
+  ["7", () => appendDisplay("7")],
+  ["8", () => appendDisplay("8")],
+  ["9", () => appendDisplay("9")],
+  ["/", () => appendDisplay("/")],
+  ["4", () => appendDisplay("4")],
+  ["5", () => appendDisplay("5")],
+  ["6", () => appendDisplay("6")],
+  ["-", () => appendDisplay("-")],
+  ["1", () => appendDisplay("1")],
+  ["2", () => appendDisplay("2")],
+  ["3", () => appendDisplay("3")],
+  ["+", () => appendDisplay("+")],
+  ["0", () => appendDisplay("0")],
+  ["00", () => appendDisplay("00")],
+  ["=", calculate],
+ 
+];
+
+//! variable to store buttons created
+let buttonsContainer = document.getElementById("buttons");
+buttons.forEach(([value, clickHandler]) => {
+  let button = createButton(value, clickHandler);
+  buttonsContainer.appendChild(button);
+});
+  
